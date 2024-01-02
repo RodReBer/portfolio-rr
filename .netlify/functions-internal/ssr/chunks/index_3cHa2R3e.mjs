78763a2abdaa06862088993173081415fec557b1
Rodrigo Rey <rodrigorey2005@gmail.com>
2024-01-02 01:47:35 -0300

    form

--- .netlify/functions-internal/ssr/chunks/index_3cHa2R3e.mjs
+++ .netlify/functions-internal/ssr/chunks/index_3cHa2R3e.mjs
@@ -1,6 +1,6 @@
 export { renderers } from '../renderers.mjs';
 export { onRequest } from '../_empty-middleware.mjs';
 
-const page = () => import('./pages/index__RJ-qMwh.mjs');
+const page = () => import('./pages/index_-XLd3jI7.mjs');
 
 export { page };
