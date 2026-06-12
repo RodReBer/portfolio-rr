84567a9d315ac1bf5d8a78d0ef979a4f98f64621
Rodrigo Rey <rodrigorey2005@gmail.com>
2026-06-11 22:02:55 -0300

    fix: update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rodrigorey.info',
   integrations: [react()],
   adapter: node({
     mode: "standalone",
